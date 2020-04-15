982a6ef5b1501c396b05a97aa649079192beed72
Brenda Fabela <brenda.fabela.enriquez@cern.ch>
2020-04-15 16:15:12 -0500

    Adding year as a command-line argument.

--- src/Analyzer.h
+++ src/Analyzer.h
@@ -52,7 +52,7 @@ public:
   ~Analyzer();
   void add_metadata(std::vector<std::string> infiles);
   void clear_values();
-  void preprocess(int); //05.28.19
+  void preprocess(int, std::string); //05.28.19
   bool fillCuts(bool);
   void printCuts();
   void writeout();
